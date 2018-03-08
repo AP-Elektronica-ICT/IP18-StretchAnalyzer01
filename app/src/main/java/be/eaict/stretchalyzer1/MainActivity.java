6452f547d0b693e29e66f64edd2c707425702501
Stijn Mets <metsstijn@gmail.com>
2018-03-08 22:24:52 +0100

    bluetooth paired devices check

--- app/src/main/java/be/eaict/stretchalyzer1/MainActivity.java
+++ app/src/main/java/be/eaict/stretchalyzer1/MainActivity.java
@@ -1,13 +1,36 @@
 package be.eaict.stretchalyzer1;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int REQUEST_ENABLE_BT = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothAdapter myBleutoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(!myBleutoothAdapter.isEnabled()){
+            Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBT, REQUEST_ENABLE_BT);
+        }
+
+        Set<BluetoothDevice> pairedDevices = myBleutoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0){
+            for(BluetoothDevice device : pairedDevices){
+                if(device.getName() == "HC-05"){
+                    Log.d("Address", device.getAddress());
+                }
+            }
+        }
     }
 }
