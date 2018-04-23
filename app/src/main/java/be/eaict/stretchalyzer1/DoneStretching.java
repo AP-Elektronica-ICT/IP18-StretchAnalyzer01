6c72a6af2e93c3c308df995e129f58c40be0a895
Unknown <midasvg@hotmail.com>
2018-04-23 14:03:47 +0200

    Close button

--- app/src/main/java/be/eaict/stretchalyzer1/DoneStretching.java
+++ app/src/main/java/be/eaict/stretchalyzer1/DoneStretching.java
@@ -1,7 +1,10 @@
 package be.eaict.stretchalyzer1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DoneStretching extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class DoneStretching extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_done_stretching);
+
+        Button closeFinal = (Button) this.findViewById(R.id.bttnClose);
+        closeFinal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DoneStretching.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
