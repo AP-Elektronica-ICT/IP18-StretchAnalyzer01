afd83c93cc17fc202eed27395d42f4c1adff346b
Unknown <midasvg@hotmail.com>
2018-04-24 11:42:01 +0200

    Pictures added with next/previous exercise

--- app/src/main/java/be/eaict/stretchalyzer1/DoneStretchingActivity.java
+++ app/src/main/java/be/eaict/stretchalyzer1/DoneStretchingActivity.java
@@ -5,13 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.github.mikephil.charting.charts.LineChart;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DoneStretchingActivity extends AppCompatActivity {
+
     List<String> graphValues;
     List<String> graphTimeStamps;
+
+    LineChart lineChart;
+
     private Map<String , List<String>> bluetoothData = new HashMap<String  , List<String>>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,7 @@ public class DoneStretchingActivity extends AppCompatActivity {
         graphTimeStamps = bluetoothData.get("TimeStamps");
         Log.d("Values", graphValues.toString());
         Log.d("TimeStamps", graphTimeStamps.toString());
+
 
     }
 }
