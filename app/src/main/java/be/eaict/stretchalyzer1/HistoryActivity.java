34b9f6765f84648b22e7ad93538f0dfda9fc8c76
Unknown <midasvg@hotmail.com>
2018-04-24 21:55:32 +0200

    HistoryActivity Graph + back button

--- app/src/main/java/be/eaict/stretchalyzer1/HistoryActivity.java
+++ app/src/main/java/be/eaict/stretchalyzer1/HistoryActivity.java
@@ -1,7 +1,10 @@
 package be.eaict.stretchalyzer1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
@@ -28,7 +31,22 @@ public class HistoryActivity extends AppCompatActivity {
 
         barChart = (BarChart) findViewById(R.id.bargraph);
         createRandomBarGraph("2016/05/05", "2016/06/01");
+
+
+        Button bttnBack = (Button) findViewById(R.id.buttonBack);
+        bttnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentHistory = new Intent(HistoryActivity.this, MainActivity.class);
+                startActivity(intentHistory);
+            }
+        });
+
     }
+
+
+
+
 
     public void createRandomBarGraph(String Date1, String Date2){
 
